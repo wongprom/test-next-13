2333ac4bddb956010112501fb16b7438fcf1204d
wongprom <>
2023-05-10 22:36:32 +0200

    Format code.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,13 +3,11 @@ import React from 'react';
 
 const about = () => {
   return (
-    
     <div className="flex flex-col items-center justify-center">
-        <h1 className="text-2xl text-white">About Page</h1>
-        <LinkButton url="/" text="Go to Home Page" />
-        <LinkButton url="/contact"  text="Go to Contact Page" />
-      </div>
-   
+      <h1 className="text-2xl text-white">About Page</h1>
+      <LinkButton url="/" text="Go to Home Page" />
+      <LinkButton url="/contact" text="Go to Contact Page" />
+    </div>
   );
 };
 

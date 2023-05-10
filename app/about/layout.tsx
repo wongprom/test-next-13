7f5da4dc4307ca2d0112a1729f62bc8518eb85a2
wongprom <>
2023-05-10 22:39:34 +0200

    Create layout for about route.

---
+++ app/about/layout.tsx
@@ -0,0 +1,19 @@
+import Navbar from '@/components/Navbar';
+import '../../styles/globals.css';
+
+export const metadata = {
+  title: 'About',
+  description: 'Generated WongP',
+};
+
+export default function AboutLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <main className="flex min-h-screen justify-center">{children}</main>
+    </div>
+  );
+}

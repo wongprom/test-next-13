d9969666e12ca265239710ca257c930b97c3d589
wongprom <>
2023-05-12 17:27:36 +0200

    Create folder withfile, add data from ADEPT 15 character test.

---
+++ data/data.ts
@@ -0,0 +1,106 @@
+const characterTestADEPT15 = [
+  {
+    title: 'Målmedvetenhet',
+    0: 'Fokuserad på att slutföra arbetet och att de strävar efter att lyckas - så länge målen är tydligt identifierade och inom räckhåll',
+    1: 'Villiga att acceptera och leverera bra, men inte perfekta, resultat för att få saker slutförda',
+    2: 'I allmänhet villiga att prioritera om vid motgångar i arbetet',
+    3: 'Att de oftast slutför arbetet i tid',
+  },
+  {
+    title: 'Struktur',
+    0: 'Att de vanligtvis kan fokusera både på detaljer och helhet',
+    1: 'Att de kan att ta sig an uppgifter på ett metodiskt och planerat sätt när det behövs',
+    2: 'Att de föredrar att vara organiserade, men brukar vara mindre organiserade under stressiga perioder',
+    3: 'Att de kan ta fram strukturerade planer när det finns behov för det',
+  },
+  {
+    title: 'Konceptuell',
+    0: 'Att de är kreativa och intellektuellt intresserade inom sina kompetensområden',
+    1: 'Att de föredrar taktiska tillvägagångssätt, men är öppna för att tänka strategiskt',
+    2: 'Att de kan hitta originella lösningar på vanliga eller kända problem, men är mindre sannolika att hitta det på större eller mer komplexa problem',
+  },
+  {
+    title: 'Flexibilitet',
+    0: 'Öppna när det gäller vissa ämnen, men rigida i sina åsikter och övertygelser när det gäller andra',
+    1: 'Villiga att förändra och anpassa sig så länge det finns en bra anledning till det',
+    2: 'Öppna för nya idéer och erfarenheter, men söker dem inte',
+    3: 'Flexibla, men föredrar oftast att saker inte ändras',
+    4: 'Att de föredrar välbeprövade metoder, så länge de fungerar hyfsat',
+  },
+  {
+    title: 'Bemästrande',
+    0: 'Att de föredrar att fokusera på befintliga styrkor, men också är villiga att utveckla nya',
+    1: 'Att de anser att individers förmågor är svåra att förändra, men vissa färdigheter kan utvecklas genom hårt arbete, träning och utbildning',
+    2: 'Intresserade av att hjälpa begåvade individer lära sig och utvecklas',
+  },
+  {
+    Ambition: {
+      0: 'Något mer intresserade av personliga mål än mål som har med karriären att göra',
+      1: 'Att de oftast har bra balans mellan arbete och privatliv',
+      2: 'Att de är något mer intresserade av att tycka om sitt jobb snarare än att bli befordrade',
+      3: 'Att de ibland föredrar uppnåeliga mål framför mycket utmanande mål i arbetet',
+    },
+  },
+  {
+    Inflytande: {
+      0: 'Bra lagspelare i de flesta situationer',
+      1: 'Att de ibland hellre leder med exempel än genom auktoritet och ansvar',
+      2: 'Att de oftast är öppna för att ta direktiv från andra',
+      3: 'Något mer bekväma med att bidra som individ än som ledare',
+    },
+  },
+  {
+    Dominans: {
+      0: 'Att de generellt sett föredrar en arbetsmiljö präglad av samarbete snarare än konkurrens',
+      1: 'Förolämpar sällan andra och uppträder sällan aggressivt',
+      2: 'Mestadels försiktiga och eftertänksamma',
+    },
+  },
+  {
+    Livlighet: {
+      0: 'Mestadels bekväma med att arbeta självständigt utan mycket social kontakt',
+      1: 'Att de i allmänhet föredrar att vara ensamma eller i små grupper av nära vänner',
+      2: 'Något mindre intresserade av stora sociala evenemang',
+    },
+  },
+  {
+    Stabilitet: {
+      0: 'Öppna med sin passion och iver',
+      1: 'Snabba på att svara, lätta att läsa av och i kontakt med sina känslor',
+    },
+  },
+  {
+    Optimism: {
+      0: 'Att de ofta utvärderar situationer noggrant och är medvetna om potentiella problem',
+      1: 'I allmänhet realistiska i sina löften',
+      2: 'Att de ibland kan vara självkritiska och ivriga att prestera bra',
+    },
+  },
+  {
+    Medvetenhet: {
+      0: 'Att de har god förståelse för hur deras beteende påverkar andra',
+      1: 'Till stor del medvetna om sina egna styrkor och svagheter',
+      2: 'Att de i allmänhet söker återkoppling',
+    },
+  },
+  {
+    title: 'Samarbetsvilja',
+    0: 'Villiga att hjälpa andra om det behövs för att teamet ska lyckas',
+    1: 'Att de oftast litar på andra, men är aningen skeptiska till en början',
+    2: 'Öppna för att be om hjälp, men undviker det gärna om det går',
+    3: 'Mestadels villiga att tillmötesgå andra',
+  },
+  {
+    title: 'Hänsynsfullhet',
+    0: 'Överlag sympatiska, stödjande och intresserade av andras problem',
+    1: 'Att de oftast föredrar att fria framför att fälla',
+    2: 'Oftast intresserade av att förstå individers beteende',
+  },
+  {
+    title: 'Anspråkslöshet',
+    0: 'I allmänhet uppriktiga, öppna och ärliga',
+    1: 'Oftast tveksamma till att ta åt sig ära för framgångar',
+  },
+];
+
+export { characterTestADEPT15 };

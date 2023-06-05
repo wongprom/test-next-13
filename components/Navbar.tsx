f570e88421e1f87bd6f67649f643487a6b76967f
wongprom <>
2023-06-05 14:54:31 +0200

    Navbar links, change text color on hover and add cursor pointer.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,10 +20,14 @@ const Navbar = () => {
           <span className="ml-3 text-xl text-[#aaaaaa]">Tailblocks</span>
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center text-white">
-          <a className="mr-5 hover:text-gray-900">First Link</a>
-          <a className="mr-5 hover:text-gray-900">Second Link</a>
-          <a className="mr-5 hover:text-gray-900">Third Link</a>
-          <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+          <a className="mr-5 hover:text-[#eeeeee] cursor-pointer">First Link</a>
+          <a className="mr-5 hover:text-[#eeeeee] cursor-pointer">
+            Second Link
+          </a>
+          <a className="mr-5 hover:text-[#eeeeee] cursor-pointer">Third Link</a>
+          <a className="mr-5 hover:text-[#eeeeee] cursor-pointer">
+            Fourth Link
+          </a>
         </nav>
       </div>
     </header>

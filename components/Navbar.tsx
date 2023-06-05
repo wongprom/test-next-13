11837bbb140b3d606431bda7215942196e50b850
wongprom <>
2023-06-05 14:50:32 +0200

    Use new text color for links and change navbar bg color.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <header className="shrink-0 bg-blue-500 body-font">
+    <header className="shrink-0 bg-[#222222] body-font text-color-[#ffffff]">
       <div className="mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <svg
@@ -17,7 +17,7 @@ const Navbar = () => {
           >
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
-          <span className="ml-3 text-xl">Tailblocks</span>
+          <span className="ml-3 text-xl text-[#aaaaaa]">Tailblocks</span>
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center text-white">
           <a className="mr-5 hover:text-gray-900">First Link</a>

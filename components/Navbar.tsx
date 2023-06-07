c5f6e989596009b8e9aa4ec67cc9344bfbbb788b
wongprom <>
2023-06-07 18:24:57 +0200

    Navbar BugFix, fix error that cause hydration error.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,10 @@ const Navbar = () => {
   return (
     <header className="shrink-0 bg-[#222222] body-font text-color-[#ffffff]">
       <div className="mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <Link
+          href={'/'}
+          className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -18,14 +21,8 @@ const Navbar = () => {
           >
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
-          <Link
-            href={'/'}
-            className="cursor-pointer ml-3 text-xl text-[#aaaaaa]"
-          >
-            Tailblocks
-          </Link>
-          {/* <span className="ml-3 text-xl text-[#aaaaaa]">Tailblocks</span> */}
-        </a>
+          <span className="ml-3 text-xl text-[#aaaaaa]">Tailblocks</span>
+        </Link>
         <nav
           className={`${mulish.className} md:ml-auto flex flex-wrap items-center text-base justify-center text-white`}
         >

36c94bd51e279ba27d937e59628ca470c187308e
wongprom <>
2023-06-07 14:00:33 +0200

    LinkButton, add no wrapp on button text.

--- components/LinkButton.tsx
+++ components/LinkButton.tsx
@@ -11,7 +11,7 @@ const LinkButton = (props: Props) => {
     <div className="m-5">
       <Link
         href={props.url}
-        className="text-center text-[#ffffff] bg-[#666666] py-2 px-8 focus:outline-none hover:bg-[#ffffff] hover:hover:text-[#666666] rounded text-lg m-4"
+        className="text-center text-[#ffffff] bg-[#666666] py-2 px-8 focus:outline-none hover:bg-[#ffffff] hover:hover:text-[#666666] rounded text-lg m-4 whitespace-nowrap"
       >
         {props.text}
       </Link>

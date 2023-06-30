652a10f844d6c018cfe034301b73f945f75143af
wongprom <>
2023-06-30 14:17:27 +0200

    Accordion, change text color when open.

--- app/about/components/Accordion.tsx
+++ app/about/components/Accordion.tsx
@@ -16,7 +16,7 @@ const Accordion = ({data}:Props) => {
     setItem({...item, active: newActive})
   }
 
-  return <div onClick={handleToggleActive} className={`w-full bg-[#666666] text-[#ffffff] p-5 border border-[#ffffff] rounded-md max-w-2xl duration-300 cursor-pointer group ${item.active === 1 ? "is-active bg-[#ffffff] text-[#666666] font-semibold": ""} `}>
+  return <div onClick={handleToggleActive} className={`w-full bg-[#666666] text-[#ffffff] p-5 border border-[#ffffff] rounded-md max-w-2xl duration-300 cursor-pointer group ${item.active === 1 ? "is-active bg-[#ffffff] text-[#666] font-semibold": ""} `}>
     <div className="flex items-center">
       <div className="w-full group-[.is-active]:font-extrabold group-[.is-active]:text-2xl group-[.is-active]:underline duration-300">{item.title}</div>
       <div  className="text-xl rotate-90 group-[.is-active]:rotate-[270deg] duration-300">></div>

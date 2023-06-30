2c2cbcbfe5e5101118f7f625107a330458908910
wongprom <>
2023-06-30 14:26:34 +0200

    Accordion, change text color when open.

--- app/about/components/Accordion.tsx
+++ app/about/components/Accordion.tsx
@@ -18,11 +18,11 @@ const Accordion = ({data}:Props) => {
 
   return <div onClick={handleToggleActive} className={`w-full bg-[#666666] text-[#ffffff] p-5 border border-[#ffffff] rounded-md max-w-2xl duration-300 cursor-pointer group ${item.active === 1 ? "is-active bg-[#ffffff] text-[#666] font-semibold": ""} `}>
     <div className="flex items-center">
-      <div className="w-full group-[.is-active]:font-extrabold group-[.is-active]:text-2xl group-[.is-active]:underline duration-300">{item.title}</div>
+      <div className="w-full group-[.is-active]:font-extrabold group-[.is-active]:text-[#666666] group-[.is-active]:text-2xl group-[.is-active]:underline duration-300">{item.title}</div>
       <div  className="text-xl rotate-90 group-[.is-active]:rotate-[270deg] duration-300">></div>
     </div>
     
-    <div className=" flex flex-col gap-4 overflow-hidden max-h-0 group-[.is-active]:max-h-max group-[.is-active]:md:text-lg duration-300">
+    <div className=" flex flex-col gap-4 overflow-hidden max-h-0 group-[.is-active]:max-h-max group-[.is-active]:text-[#666666] group-[.is-active]:md:text-lg duration-300">
       {item.info.map(inf => <p>{inf}</p>)}
     </div>
   </div>;

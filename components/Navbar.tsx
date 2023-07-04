595dcf3a8c2425d24c29d6d51cddb50a90f16517
wongprom <>
2023-07-04 10:09:02 +0200

    NEW LAYOUT NAVBAR, add styling for mobile screen.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,28 @@
 'use client';
-import Link from 'next/link';
 import React, { useState } from 'react';
-import { Bars3Icon } from '@heroicons/react/24/solid';
-import { UserCircleIcon } from '@heroicons/react/24/solid';
-import { XMarkIcon } from '@heroicons/react/24/solid';
+import Link from 'next/link';
+import {
+  XMarkIcon,
+  UserCircleIcon,
+  Bars3Icon,
+  MoonIcon,
+} from '@heroicons/react/24/solid';
+import {
+  BookOpenIcon,
+  CameraIcon,
+  DocumentIcon,
+  FolderIcon,
+  MusicalNoteIcon,
+} from '@heroicons/react/24/outline';
 import { mulish } from '../app/fonts/fonts';
+
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   console.log('🚀 ~ file: Navbar.tsx:9 ~ Navbar ~ open:', open);
+
+  const handlerClick = () => {
+    setOpen(false);
+  };
 
   return (
     <header className="relative">
@@ -17,7 +32,7 @@ const Navbar = () => {
         } shrink-0 bg-[#222222] body-font text-[#ffffff]`}
       >
         <div className="flex justify-between">
-          <div className="flex items-center">
+          <div className="flex items-center pt-4">
             <UserCircleIcon className="h-6 w-6" />
             <div className="pl-3 text-xs flex flex-col justify-center">
               <p className="font-bold">Jimmy Bäckström</p>
@@ -55,41 +70,115 @@ const Navbar = () => {
           >
             <Link
               href={'/'}
-              className="mr-5 hover:text-[#eeeeee] cursor-pointer"
+              className="
+               hover:text-[#eeeeee] cursor-pointer"
             >
               Home
             </Link>
             <Link
               href={'/about'}
-              className="mr-5 hover:text-[#eeeeee] cursor-pointer"
+              className=" hover:text-[#eeeeee] cursor-pointer"
             >
               About
             </Link>
-            <a className="mr-5 hover:text-[#eeeeee] cursor-pointer">
-              Third Link
-            </a>
-            <a className="mr-5 hover:text-[#eeeeee] cursor-pointer">
-              Fourth Link
-            </a>
           </nav>
         </div>
       </div>
       <div
         className={`${
           !open && 'hidden '
-        } absolute top-0 left-0 h-screen w-full bg-slate-300 z-100`}
+        } fixed h-screen w-full bg-[#121316] text-[#696F7D] z-20`}
       >
-        <div className={'relative'}>
+        <div className="text-[#E5E6E9]">
           <XMarkIcon
             onClick={() => setOpen(false)}
-            className="h-12 w-12 absolute top-2 right-2 cursor-pointer"
+            className="h-12 w-12 absolute top-2 right-2 cursor-pointer z-30"
           />
-
+        </div>
+        <div className="flex h-full flex-col gap-14 py-11 px-8">
           <ul>
-            <ul>länk1</ul>
-            <ul>länk2</ul>
-            <ul>länk3</ul>
+            <p className="">WHAT I CREATE</p>
+            <li className="flex items-center pt-4">
+              <FolderIcon className="h-5 w-5" />
+              <Link
+                href={'/about'}
+                className=" hover:text-[#eeeeee] cursor-pointer pl-2 text-[#E5E6E9]"
+                onClick={handlerClick}
+              >
+                Projects
+              </Link>
+            </li>
+            <li className="flex items-center pt-4">
+              <CameraIcon className="h-5 w-5" />
+              <Link
+                href={'/about'}
+                className=" hover:text-[#eeeeee] cursor-pointer pl-2 text-[#E5E6E9]"
+                onClick={handlerClick}
+              >
+                Photography
+              </Link>
+            </li>
           </ul>
+          <ul className="">
+            <p className="text-[#696F7D]">WHAT I CONSUME</p>
+            <li className="flex items-center pt-4 ">
+              <BookOpenIcon className="h-5 w-5" />
+              <Link
+                href={'/about'}
+                className=" hover:text-[#eeeeee] cursor-pointer pl-2 text-[#E5E6E9]"
+                onClick={handlerClick}
+              >
+                Books
+              </Link>
+            </li>
+            <li className="flex items-center pt-4">
+              <MusicalNoteIcon className="h-5 w-5" />
+              <Link
+                href={'/about'}
+                className=" hover:text-[#eeeeee] cursor-pointer pl-2 text-[#E5E6E9]"
+                onClick={handlerClick}
+              >
+                Music
+              </Link>
+            </li>
+          </ul>
+          <ul className="">
+            <p className="text-[#696F7D]">WHERE TO FIND ME</p>
+            <li className="flex items-center pt-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="#E5E6E9"
+              >
+                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+              </svg>
+              <Link
+                href={'/about'}
+                className=" hover:text-[#eeeeee] cursor-pointer pl-2 text-[#E5E6E9]"
+                onClick={handlerClick}
+              >
+                Github
+              </Link>
+            </li>
+            <li className="flex items-center pt-4">
+              <DocumentIcon className="h-5 w-5" />
+
+              <Link
+                href={'/about'}
+                className=" hover:text-[#eeeeee] cursor-pointer pl-2 text-[#E5E6E9]"
+                onClick={handlerClick}
+              >
+                ReadCV
+              </Link>
+            </li>
+          </ul>
+          <div className="mt-auto flex justify-center">
+            <button className="flex justify-center p-4 w-full rounded-lg cursor-pointer text-[#e5e6e9]  bg-[#23252a]">
+              <MoonIcon className="h-4 w-4" />
+            </button>
+          </div>
         </div>
       </div>
     </header>

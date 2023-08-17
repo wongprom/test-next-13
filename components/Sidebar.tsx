a57acee4afa5aa3b8bc00f4cded27aa8ccc8dd7c
wongprom <>
2023-08-17 14:29:52 +0200

    #4 .Add Package react-tooltip...
    display tooltip when hover on icons in navbar on smaler screens.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,6 +9,7 @@ import {
   MusicalNoteIcon,
   ArrowUpRightIcon,
 } from '@heroicons/react/24/outline';
+import { Tooltip } from 'react-tooltip';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import React from 'react';
@@ -26,7 +27,18 @@ const Sidebar = () => {
             currentRoute === '/' && activeLinkStyle
           } cursor-pointer`}
         >
-          <div className="text-center">loggo</div>
+          <div
+            className="text-center"
+            data-tooltip-id="home-tooltip"
+            data-tooltip-content="Home"
+          >
+            loggo
+          </div>
+          <Tooltip
+            id="home-tooltip"
+            place="right"
+            className="xl:hidden text-sm font-normal rounded-lg tracking-wider"
+          />
         </Link>
 
         <div className="flex flex-col items-center  space-y-2">
@@ -36,7 +48,16 @@ const Sidebar = () => {
               currentRoute === '/about' && activeLinkStyle
             } cursor-pointer`}
           >
-            <FaceSmileIcon className="h-6 w-6" />
+            <FaceSmileIcon
+              className="h-6 w-6"
+              data-tooltip-id="face-smile-icon-tooltip"
+              data-tooltip-content="About"
+            />
+            <Tooltip
+              id="face-smile-icon-tooltip"
+              place="right"
+              className="xl:hidden text-sm font-normal rounded-lg tracking-wider"
+            />
           </Link>
         </div>
 
@@ -50,7 +71,16 @@ const Sidebar = () => {
                 currentRoute === '/projects' && activeLinkStyle
               } cursor-pointer`}
             >
-              <FolderIcon className="h-6 w-6" />
+              <FolderIcon
+                className="h-6 w-6"
+                data-tooltip-id="folder-icon-tooltip"
+                data-tooltip-content="Projects"
+              />
+              <Tooltip
+                id="folder-icon-tooltip"
+                place="right"
+                className="xl:hidden text-sm font-normal rounded-lg tracking-wider"
+              />
             </Link>
             <p className="hidden xl:block ml-2">Project</p>
             <div className="hidden p-1 xl:flex items-center justify-center ml-auto text-[#828996] bg-[#23252a] text-[10px] h-4 w-4 rounded">
@@ -60,7 +90,16 @@ const Sidebar = () => {
 
           <div className="flex w-full items-center justify-center">
             <Link href={'/'} className="cursor-pointer">
-              <CameraIcon className="h-6 w-6" />
+              <CameraIcon
+                className="h-6 w-6"
+                data-tooltip-id="camera-icon-tooltip"
+                data-tooltip-content="Photography"
+              />
+              <Tooltip
+                id="camera-icon-tooltip"
+                place="right"
+                className="xl:hidden text-sm font-normal rounded-lg tracking-wider"
+              />
             </Link>
             <p className="hidden xl:block ml-2">Photography</p>
             <div className="hidden p-1 xl:flex items-center justify-center ml-auto text-[#828996] bg-[#23252a] text-[10px] h-4 w-4 rounded">
@@ -72,7 +111,16 @@ const Sidebar = () => {
           <p className="hidden xl:block capitalize">what i consume</p>
           <div className="flex w-full items-center justify-center">
             <Link href={'/'} className="cursor-pointer">
-              <BookOpenIcon className="h-6 w-6" />
+              <BookOpenIcon
+                className="h-6 w-6"
+                data-tooltip-id="book-open-icon-tooltip"
+                data-tooltip-content="Books"
+              />
+              <Tooltip
+                id="book-open-icon-tooltip"
+                place="right"
+                className="xl:hidden text-sm font-normal rounded-lg tracking-wider"
+              />
             </Link>
             <p className="hidden xl:block ml-2">Books</p>
             <div className="hidden p-1 xl:flex items-center justify-center ml-auto text-[#828996] bg-[#23252a] text-[10px] h-4 w-4 rounded">
@@ -82,7 +130,16 @@ const Sidebar = () => {
 
           <div className="flex w-full items-center justify-center">
             <Link href={'/'} className="cursor-pointer">
-              <MusicalNoteIcon className="h-6 w-6" />
+              <MusicalNoteIcon
+                className="h-6 w-6"
+                data-tooltip-id="musical-note-icon-tooltip"
+                data-tooltip-content="Music"
+              />
+              <Tooltip
+                id="musical-note-icon-tooltip"
+                place="right"
+                className="xl:hidden text-sm font-normal rounded-lg tracking-wider"
+              />
             </Link>
             <p className="hidden xl:block ml-2">Music</p>
             <div className="hidden p-1 xl:flex items-center justify-center ml-auto text-[#828996] bg-[#23252a] text-[10px] h-4 w-4 rounded">
@@ -100,9 +157,16 @@ const Sidebar = () => {
                 height="15"
                 viewBox="0 0 24 24"
                 fill="#9ea3ad"
+                data-tooltip-id="github-icon-tooltip"
+                data-tooltip-content="Github"
               >
                 <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
               </svg>
+              <Tooltip
+                id="github-icon-tooltip"
+                place="right"
+                className="xl:hidden text-sm font-normal rounded-lg tracking-wider"
+              />
             </Link>
             <p className="hidden xl:block ml-2">Github</p>
             <div className="hidden p-1 xl:flex items-center justify-center ml-auto text-[#828996] bg-[#121316] text-[10px] h-5 w-5 rounded">

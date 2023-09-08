bf4d859e1a2e97edc82b885cdd3c7ca12179750e
wongprom <>
2023-09-08 11:43:45 +0200

    #8 CertificateCard, add capitalize on title.

--- components/CertificateCard.tsx
+++ components/CertificateCard.tsx
@@ -12,7 +12,7 @@ const CertificateCard = ({ slug, subTitle, infoText, title }: Props) => {
   return (
     <div className="bg-[#2E2E41] min-w-min text-slate-50 rounded-lg p-6 max-w-sm odd:self-end shadow-xl z-10">
       <div className="sm:flex justify-between items-center">
-        <p className=" pb-1">{title}</p>
+        <p className="capitalize pb-1">{title}</p>
         <p className=" text-[#CD835F] text-xs">{subTitle}</p>
       </div>
       <p className="text-xs font-mono">{infoText}</p>

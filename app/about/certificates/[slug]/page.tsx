ca0dcd18dd44a09a95721e228e6cf17bb6fb65b2
wongprom <>
2023-09-11 14:15:52 +0200

    #8 Display hlr image on slug.

--- app/about/certificates/[slug]/page.tsx
+++ app/about/certificates/[slug]/page.tsx
@@ -1,15 +1,8 @@
 import { testCertificates } from '../../../../data/data';
 import React from 'react';
+import Image from 'next/image';
 
 export const dynamicParams = false;
-
-export type Certificate = {
-  slug: string;
-  title: string;
-  subTitle: string;
-  infoText: string;
-};
-
 export async function generateStaticParams() {
   const allCert = testCertificates;
 
@@ -17,12 +10,47 @@ export async function generateStaticParams() {
     slug: cert.slug,
   }));
 }
+
+export type Certificate = {
+  slug: string;
+};
+
 const CertificatePage = ({ params }: { params: Certificate }) => {
   const { slug } = params;
+  console.log('🚀 ~ file: page.tsx:20 ~ CertificatePage ~ slug:', slug);
   return (
-    <div className="text-white">
-      <p className="text-4xl ">CertificatePage</p>
-      <p>Param slug: {slug}</p>
+    <div className="flex flex-col items-center justify-center text-white bg-[#2A2A3B] p-0 sm:p-8 ">
+      <div className=" max-w-screen-lg">
+        <div className="w-full flex items-center justify-center mb-8">
+          <Image
+            src={'/hlr.png'}
+            alt="certificate"
+            width="768"
+            height="865"
+            priority={true}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw"
+            style={{ width: 'auto', height: 'auto' }}
+          />
+        </div>
+        <div className="grid grid-cols-4 gap-2">
+          <div className="col-span-4 lg:col-span-1">
+            <h1 className="text-3xl text-center sm:text-left lg:text-2xl">
+              Certificates
+            </h1>
+          </div>
+          <div className="col-span-4 lg:col-span-3 mt-2">
+            <p className="text-xl">
+              Visit my certificate collection, where new qualifications
+              magically appear like rabbits from a magician&apos;s hat – you
+              never know what impressive trick I&apos;ll pull off next! Keep
+              checking in, it&apos;s like unwrapping surprise gifts of
+              expertise.
+            </p>
+          </div>
+        </div>
+
+        <p>some thing</p>
+      </div>
     </div>
   );
 };

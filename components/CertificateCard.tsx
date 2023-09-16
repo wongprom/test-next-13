dcf1faa1fd8c6b58a9b23c40fe84ab6d64a91af0
wongprom <>
2023-09-16 12:39:14 +0200

    #9 Add line-clamp to category info text.

--- components/CertificateCard.tsx
+++ components/CertificateCard.tsx
@@ -17,7 +17,9 @@ const CertificateCard = ({ slug, subTitle, infoText, title }: Props) => {
         <p className="capitalize pb-1">{title}</p>
         <p className=" text-[#CD835F] text-xs">{subTitle}</p>
       </div>
-      <PortableText value={infoText} />
+      <div className="line-clamp-2">
+        <PortableText value={infoText} />
+      </div>
       <Link className="" href={`/about/certificates/${slug}`}>
         <button className="px-6 py-4 bg-[#F09666] text-xs mt-4 text-white rounded-sm hover:text-[#F09666] hover:bg-white">
           View Certificate

a74889536f4ce3b44f9e21f2510b72a82499ef1f
wongprom <>
2023-09-19 22:28:26 +0200

    #9 Remove schemas not used...
     author, category, post

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,7 +1,3 @@
-// import blockContent from './blockContent';
-// import category from './category';
-// import post from './post';
-// import author from './author';
 import certificate from './certificate-schema';
 
 const schemas = [certificate];

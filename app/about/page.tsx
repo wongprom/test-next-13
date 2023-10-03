329728dfada902fa0e5251554522a1386ca53c66
wongprom <>
2023-10-03 11:39:56 +0200

    #38 Add real text for About route.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,9 +2,36 @@ import React from 'react';
 
 const about = () => {
   return (
-    <p className="text-[#babdc5]">
-      Some text about what to expect on this about page with nested routes Right
-    </p>
+    <div className="text-white bg-[#2A2A3B] p-0 sm:p-8">
+      <div className="max-w-screen-lg mx-auto">
+        <div className="grid grid-cols-4 gap-2">
+          <div className="col-span-4 lg:col-span-1">
+            <h1 className="text-3xl text-center sm:text-left lg:text-2xl">
+              About
+            </h1>
+          </div>
+          <div className="col-span-4 lg:col-span-3 mt-2">
+            <p>
+              The Inside Scoop on the Developer and Friend Welcome to the
+              subdomains that reveal the multifaceted me – both as a developer
+              and a friend. Here, I aim to offer you a sneak peek into the
+              essence of who I am, whether as a trusted colleague or a buddy for
+              life. This page isn&apos;t just about me; it&apos;s about
+              cultivating a mutual understanding. Why? Because I truly believe
+              in the magic of teamwork, where each member contributes their
+              unique positive energy. It&apos;s about thriving together, both in
+              the vibrant exchanges of ideas and in the warm camaraderie that
+              makes work and life so fulfilling. So, let&apos;s dive in and
+              uncover the intricacies of this dynamic persona!
+            </p>
+          </div>
+        </div>
+
+        <div className="relative flex flex-col gap-7 bg-[#2A2A3B] max-w-screen-lg py-5 mt-5">
+          Some other content gose here
+        </div>
+      </div>
+    </div>
   );
 };
 

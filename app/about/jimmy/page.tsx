0fe80447655e4972167e60ea3ce0571fbe71040c
wongprom <>
2023-10-04 14:25:44 +0200

    #38 Use same layout as AboutPage for the top section.

--- app/about/jimmy/page.tsx
+++ app/about/jimmy/page.tsx
@@ -1,39 +1,63 @@
 import React from 'react';
+import { testAboutData } from '@/data/data';
 
 const page = () => {
+  const testDataJimmy = testAboutData.find((data) => data.title == 'Jimmy');
+
   return (
-    <p className="text-[#babdc5]">
-      1. My Journey as a Buddhist Monk in Thailand I had a unique experience
-      during my time as a monk in Thailand, which left a lasting impact on my
-      life. I found myself at two distinct types of temples, one nestled within
-      the bustling city and the other hidden deep within the tranquil forest.
-      The reason behind this spiritual journey was deeply rooted in Thai
-      culture; it&apos;s a tradition for boys with at least one parent from
-      Thailand to seek ordination as a monk. It&apos;s a way for us to apologize
-      to our parents for any pain we might have caused them throughout the years
-      and to express our heartfelt gratitude for the sacrifices they&apos;ve
-      made on our behalf. 2. My Path to Graduation in 2019: A Tech Enthusiast
-      with a Purpose In 2017, I embarked on an educational journey that led me
-      to graduate in 2019. I chose this path not only because I believe
-      it&apos;s the future but also because I yearned to work on applications
-      that could truly make a difference for each user. I was motivated by a
-      desire to solve real-world problems, whether it was simplifying the
-      process of moving from point A to point B or being a part of a solution
-      that addresses the environmental challenges we face today. Technology, to
-      me, is a tool for creating positive change. 3. Balancing Life: From the
-      Outdoors to Family and Friends In my private life, I find solace in the
-      great outdoors. There&apos;s nothing quite like setting up my 4-season
-      tent in the midst of a winter wilderness and simply being one with nature.
-      I also have a passion for calisthenics, pushing my physical limits to
-      achieve personal growth. Taking cold baths when the opportunity arises is
-      another invigorating pursuit. However, my heart belongs to my family,
-      which includes my wife and two older children. They are my anchor, and I
-      relish every moment spent with them. And when they&apos;ve had their fill
-      of my company, I cherish the times I spend with friends, often meeting up
-      at a restaurant for a hearty burger and a refreshing beer. It&apos;s in
-      these moments that I truly appreciate the balance of my life, from
-      introspective solitude to the joy of companionship.
-    </p>
+    <div className="text-white bg-[#2A2A3B] p-0 sm:p-8">
+      <div className="max-w-screen-lg mx-auto">
+        <div className="grid grid-cols-4 gap-2">
+          <div className="col-span-4 lg:col-span-1">
+            <h1 className="text-3xl text-center sm:text-left lg:text-2xl">
+              {testDataJimmy?.title}
+            </h1>
+          </div>
+          <div className="col-span-4 lg:col-span-3 mt-2">
+            <p>{testDataJimmy?.text}</p>
+          </div>
+        </div>
+        <section className="w-full">
+          <div className=" py-24 mx-auto">
+            <div className="grid gap-4 grid-cols-1 lg:grid-cols-2">
+              some other content
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+    // <p className="text-[#babdc5]">
+    //   1. My Journey as a Buddhist Monk in Thailand I had a unique experience
+    //   during my time as a monk in Thailand, which left a lasting impact on my
+    //   life. I found myself at two distinct types of temples, one nestled within
+    //   the bustling city and the other hidden deep within the tranquil forest.
+    //   The reason behind this spiritual journey was deeply rooted in Thai
+    //   culture; it&apos;s a tradition for boys with at least one parent from
+    //   Thailand to seek ordination as a monk. It&apos;s a way for us to apologize
+    //   to our parents for any pain we might have caused them throughout the years
+    //   and to express our heartfelt gratitude for the sacrifices they&apos;ve
+    //   made on our behalf. 2. My Path to Graduation in 2019: A Tech Enthusiast
+    //   with a Purpose In 2017, I embarked on an educational journey that led me
+    //   to graduate in 2019. I chose this path not only because I believe
+    //   it&apos;s the future but also because I yearned to work on applications
+    //   that could truly make a difference for each user. I was motivated by a
+    //   desire to solve real-world problems, whether it was simplifying the
+    //   process of moving from point A to point B or being a part of a solution
+    //   that addresses the environmental challenges we face today. Technology, to
+    //   me, is a tool for creating positive change. 3. Balancing Life: From the
+    //   Outdoors to Family and Friends In my private life, I find solace in the
+    //   great outdoors. There&apos;s nothing quite like setting up my 4-season
+    //   tent in the midst of a winter wilderness and simply being one with nature.
+    //   I also have a passion for calisthenics, pushing my physical limits to
+    //   achieve personal growth. Taking cold baths when the opportunity arises is
+    //   another invigorating pursuit. However, my heart belongs to my family,
+    //   which includes my wife and two older children. They are my anchor, and I
+    //   relish every moment spent with them. And when they&apos;ve had their fill
+    //   of my company, I cherish the times I spend with friends, often meeting up
+    //   at a restaurant for a hearty burger and a refreshing beer. It&apos;s in
+    //   these moments that I truly appreciate the balance of my life, from
+    //   introspective solitude to the joy of companionship.
+    // </p>
   );
 };
 

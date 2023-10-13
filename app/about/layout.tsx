9d3cdba5c5c963b9dbf7f836881b7a5352e5e8b8
wongprom <>
2023-10-13 10:47:48 +0200

    #35 Create array with links...
    map array with links.

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -11,46 +11,43 @@ export default function AboutLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const routeLinks = [
+    {
+      route: '/about',
+      linkText: 'About',
+    },
+    {
+      route: '/about/jimmy',
+      linkText: 'Jimmy',
+    },
+    {
+      route: '/about/adept',
+      linkText: 'Adept',
+    },
+    {
+      route: '/about/testimonial',
+      linkText: 'Testimonial',
+    },
+    {
+      route: '/about/certificates',
+      linkText: 'Certificates',
+    },
+  ];
   return (
     <div className="flex-1 flex flex-col p-10">
       <div className="flex gap-4">
         <div className="hidden md:flex flex-col gap-4 text-xl  md:rounded-md p-5 bg-[#aaaaaa]">
-          <h2 className="underline underline-offset-4 uppercase">
-            <Link href={'/about'}>About</Link>
-          </h2>
           <ul className="flex flex-1 flex-col gap-5 text-[#ffffff]">
-            <li>
-              <Link
-                href={'/about/jimmy'}
-                className="cursor-pointer hover:text-gray-600"
-              >
-                Jimmy
-              </Link>
-            </li>
-            <li>
-              <Link
-                href={'/about/adept'}
-                className="cursor-pointer hover:text-gray-600"
-              >
-                Adept
-              </Link>
-            </li>
-            <li>
-              <Link
-                href={'/about/testimonial'}
-                className="cursor-pointer hover:text-gray-600"
-              >
-                Testimonial
-              </Link>
-            </li>
-            <li>
-              <Link
-                href={'/about/certificates'}
-                className="cursor-pointer hover:text-gray-600"
-              >
-                Certificates
-              </Link>
-            </li>
+            {routeLinks.map((routeLink) => (
+              <li key={routeLink.linkText}>
+                <Link
+                  href={routeLink.route}
+                  className="cursor-pointer hover:text-gray-600"
+                >
+                  {routeLink.linkText}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
         <div className="flex-1 md:rounded-md border-4 border-[#aaaaaa] p-5 overflow">

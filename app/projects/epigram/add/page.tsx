1d887e25f89dee2bb57dbae35791f8c67bbb0a45
wongprom <>
2023-11-14 17:01:32 +0100

    #27 Init Shadcn and try Button...
     component. Change to use client component, will have a form that logged in user can add new quot.

--- app/projects/epigram/add/page.tsx
+++ app/projects/epigram/add/page.tsx
@@ -1,20 +1,34 @@
+'use client';
 import React from 'react';
-import { UserButton, currentUser } from '@clerk/nextjs';
+import { UserButton, currentUser, useUser } from '@clerk/nextjs';
+import { Button } from '@/components/shadcn/ui/button';
 
-const AddPage = async () => {
-  const user = await currentUser();
-  if (!user) return <div>Not logged in</div>;
-  return (
-    <div className="text-white">
-      <div className="text-4xl">
-        <p>Add Page</p>
+const AddPage = () => {
+  // const user = await currentUser();
+  const { isSignedIn, user, isLoaded } = useUser();
+  console.log('🚀 ~ file: page.tsx:8 ~ AddPage ~ user:', user);
+
+  if (!isLoaded) {
+    return <p>CLERK IS LOADING......</p>;
+  }
+  if (isSignedIn) {
+    return (
+      <div className="text-white">
+        <div className="text-4xl">
+          <p>Add Page</p>
+        </div>
+        <Button variant="destructive">test</Button>
+        <div className="flex">
+          <UserButton afterSignOutUrl="/projects/epigram" />{' '}
+          <p>
+            Hello User:{' '}
+            {user.firstName || user.username || user.fullName || user.lastName}
+          </p>
+        </div>
       </div>
-      <div className="flex">
-        <UserButton afterSignOutUrl="/projects/epigram" />{' '}
-        <p>Hello user: {user.username || user.firstName || user.lastName}</p>
-      </div>
-    </div>
-  );
+    );
+  }
+  return <div>Not signed in</div>;
 };
 
 export default AddPage;

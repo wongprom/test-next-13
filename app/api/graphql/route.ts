58da976f01f1e8257b962199b2374ff93af072bb
wongprom <>
2023-11-29 17:27:40 +0100

    #34 Can get data and nested data from Supabase.

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,17 +1,53 @@
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { gql } from 'graphql-tag';
-import prisma from '../../../prisma/db';
+import { prisma } from '../../../prisma/db';
+import { PrismaClient } from '@prisma/client';
+import { NextRequest } from 'next/server';
+
+export type Context = {
+  prisma: PrismaClient;
+};
 
 const resolvers = {
   Query: {
-    hello: () => 'world',
+    novels: async (parent: any, args: any, context: Context) => {
+      return await context.prisma.novel.findMany();
+    },
+    novel: async (parent: any, args: any, context: Context) => {
+      return await context.prisma.novel.findUnique({
+        where: {
+          id: args.id,
+        },
+      });
+    },
+  },
+  Novel: {
+    authors: async (parent: any, args: any, context: Context) => {
+      return await context.prisma.author.findMany({
+        where: { novelId: parent.id },
+      });
+    },
   },
 };
 
-const typeDefs = `#graphql
+export const typeDefs = gql`
+  type Novel {
+    id: ID!
+    title: String
+    image: String
+    createdAt: String
+    updatedAt: String
+    authors: [Author]
+  }
+  type Author {
+    id: ID!
+    name: String
+    novelID: String
+  }
   type Query {
-    hello: String
+    novel(id: ID!): Novel
+    novels: [Novel]
   }
 `;
 
@@ -20,12 +56,8 @@ const server = new ApolloServer({
   typeDefs,
 });
 
-// info You may also pass a context function to startServerAndCreateNextHandler as such:
-// export default startServerAndCreateNextHandler(server, {
-//   context: async (req, res) => ({ req, res, user: await getLoggedInUser(req) }),
-// });
-
-// export default startServerAndCreateNextHandler(server);
-const handler = startServerAndCreateNextHandler(server);
+const handler = startServerAndCreateNextHandler(server, {
+  context: async (req, res) => ({ req, res, prisma }),
+});
 
 export { handler as GET, handler as POST };

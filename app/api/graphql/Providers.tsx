62879fa23f845b797e2fad3d8a489c523dd59d97
wongprom <>
2023-12-18 11:18:13 +0100

    #34 ERROR, Connection refused...
     change to SUPABASE_URL.

--- app/api/graphql/Providers.tsx
+++ app/api/graphql/Providers.tsx
@@ -10,7 +10,7 @@ import { ReactNode } from 'react';
 
 export const Providers = ({ children }: { children: ReactNode }) => {
   const client = new ApolloClient({
-    uri: 'https://test-next-13-hzqlpdy00-wongprom.vercel.app/api/graphql',
+    uri: process.env.SUPABASE_URL + '/api/graphql',
     cache: new InMemoryCache(),
   });
   return <ApolloProvider client={client}>{children}</ApolloProvider>;

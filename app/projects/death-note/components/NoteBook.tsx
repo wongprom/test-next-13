afa57e6516a34ce20315e8bae032d936caa16ccd
wongprom <>
2024-03-12 22:14:16 +0100

    #44 Create OwnerPage, add test data...
    Create OwnerPage that will contain user info for Death note. Add data for Zodiac Signs in Japanese to display in OwnerPage.

--- app/projects/death-note/components/NoteBook.tsx
+++ app/projects/death-note/components/NoteBook.tsx
@@ -65,6 +65,41 @@ const RulePage = React.forwardRef((props: any, ref: any) => {
 });
 RulePage.displayName = 'RulePage';
 
+const OwnerPage = React.forwardRef((props: any, ref: any) => {
+  return (
+    <div
+      className="page font-deathNote bg-death-note-blank-page bg-cover bg-center p-10"
+      ref={ref}
+    >
+      <div className="page-content w-full h-full flex flex-col overflow-y-auto">
+        <h2 className="page-header text-5xl text-center pt-10">Death Note</h2>
+        <br />
+        {/* <div className="page-image bg-yellow-200"></div> */}
+        <p>Name: Jimmy Backstrom</p>
+        <p>Alias: Wongprom</p>
+        <p>Age: 42</p>
+        <p>Gender: Man</p>
+        <p>Your Shinigami: Ryuk</p>
+        <p>Likes:...</p>
+        <p>Disslikes:...</p>
+        <p>Main wish:...</p>
+        <p>Zodiac: Rooster (tori)</p>
+        <p>Personality based of your Zodiac:...</p>
+        <h3 className="text-3xl uppercase underline decoration-gray-400 tracking-wide">
+          {}
+        </h3>
+        <br />
+        <div className="page-text">{props.children}</div>
+        <div className="page-text text-3xl text-gray-400">{}</div>
+        <div className="page-footer mt-auto mx-auto">{`-- ${Number(
+          props.pageNumber
+        )} --`}</div>
+      </div>
+    </div>
+  );
+});
+OwnerPage.displayName = 'OwnerPage';
+
 const NoteBook = () => {
   return (
     // @ts-ignore
@@ -86,6 +121,7 @@ const NoteBook = () => {
       // className="demo-book"
     >
       <Cover>Doo it...</Cover>
+      <OwnerPage />
       {testDeathNoteRules.map((rule, index) => {
         return <RulePage pageNumber={index + 1} rule={rule}></RulePage>;
       })}

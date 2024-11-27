fd73f5dc5484950814267322b6f7a2915f38893a
wongprom <>
2024-11-27 15:35:47 +0100

    Add environment-based URI selection for Apollo Client to switch between local and production.

--- app/api/graphql/Providers.tsx
+++ app/api/graphql/Providers.tsx
@@ -1,20 +1,29 @@
-'use client';
+"use client";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   gql,
-} from '@apollo/client';
+} from "@apollo/client";
 
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 const testBranchVercel =
-  'https://test-next-13-git-34-idea-quotes-advice-wongprom.vercel.app/api/graphql';
+  "https://test-next-13-git-34-idea-quotes-advice-wongprom.vercel.app/api/graphql";
 
-const localBranch = 'http://localhost:3000/api/graphql';
+const localBranch = "http://localhost:3000/api/graphql";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const getGraphqlUri = () => {
+  if (
+    process.env.NODE_ENV === "development" ||
+    !process.env.NEXT_PUBLIC_SUPABASE_URL
+  ) {
+    return localBranch;
+  }
+  return supabaseUrl;
+};
 export const Providers = ({ children }: { children: ReactNode }) => {
   const client = new ApolloClient({
-    uri: localBranch,
+    uri: getGraphqlUri(),
     cache: new InMemoryCache(),
   });
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
